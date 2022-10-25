eaa0f628af0d2a1403f44294ee6e4849af646dca
Ciepol <adamulicho@gmail.com>
2022-10-25 23:51:18 +0200

    MealsIteam ready

--- ReactFood/src/App.js
+++ ReactFood/src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
- 
+
 export default App;
--- ReactFood/src/components/Layout/Header.js
+++ ReactFood/src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
