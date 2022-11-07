0db0b03f17654d04384aa46f7eb879e0e92d4311
Ciepol <adamulicho@gmail.com>
2022-11-07 11:16:26 +0100

    classBasedComp

--- classBasedComp/src/components/UserFinder.js
+++ classBasedComp/src/components/UserFinder.js
@@ -2,36 +2,37 @@ import { Fragment, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
       filteredUsers: [],
-      searchTerm:""
+      searchTerm: "",
     };
   }
+
   componentDidMount() {
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   // remember that DidUpdate can make infinite loop!
-  
-  componentDidUpdate(prevProps,prevState) {
+
+  componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm)
-    this.setState({
-      filteredUsers: DUMMY_USERS.filter((user) =>
-        user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
-      ),
-    });
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+        ),
+      });
   }
   searchChangeHandler = (event) => {
-        this.setState({searchTerm: event.target.value});}
+    this.setState({ searchTerm: event.target.value });
+  };
   // ComponentDidUnmount(){}
   render() {
     return (
@@ -39,7 +40,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
