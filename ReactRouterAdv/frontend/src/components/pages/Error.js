cda10c8450d70b796f6b41707ed63a2eeca08ac4
Ciepol <adamulicho@gmail.com>
2023-01-31 18:37:50 +0100

    jsonErrorHandler

--- ReactRouterAdv/frontend/src/components/pages/Error.js
+++ ReactRouterAdv/frontend/src/components/pages/Error.js
@@ -10,12 +10,23 @@ import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
 const error =useRouteError();
+let title = 'An error occurred!';
+  let message = 'Something went wrong!';
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page.';
+  }
   return (
     <>
       <MainNavigation />
       <Centered>
-        <PageContent title="an error occurred!">
-        <p>Could not find this page!</p>
+        <PageContent title={title}>
+        <p>{message}</p>
         </PageContent>
       </Centered>
     </>
--- ReactRouterAdv/frontend/src/components/pages/Events.js
+++ ReactRouterAdv/frontend/src/components/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom'
+import { json, useLoaderData } from 'react-router-dom'
 import { useContext } from 'react'
 import EventsList from '../EventsList'
 import EventContext from '../store/eventsContext'
@@ -15,19 +15,19 @@ export default function EventsPage() {
   )
 }
 
-
 //this executes on browser, not server!!
 export async function eventLoader() {
   const response = await fetch('http://localhost:8080/events')
 
   if (!response.ok) {
     // setError('Fetching events failed.');
-//     throw new Response(
-// JSON.stringify(
+    //     throw new Response(
+    // JSON.stringify(
 
-//   {message: 'could not fetch events'}
+    //   {message: 'could not fetch events'}
 
-//     )
+    //     )
+    throw json({ message: 'could not fetch events.' }, { status: 500 })
   } else {
     //we dont need to manually extract data from the response, we can return the response
     return response
